339102d1855312f390226ed24563ae0e8f7d9a5a
Melis Portakal <137406380+Melissportakall@users.noreply.github.com>
2023-12-11 11:14:33 +0300

    Update ft_printf.c
    update printf

--- ft_printf.c
+++ ft_printf.c
@@ -14,24 +14,24 @@
 #include "ft_printf.h"
 #include <stdarg.h>
 
-static	int	ft_format(va_list *arg, char format)
+static	int	ft_format_check(va_list *arg, char format)
 {
 	if (format == 'c')
 		return (ft_putchar(va_arg((*arg), int)));
 	else if (format == 's')
 		return (ft_string(va_arg((*arg), char *)));
 	else if (format == 'p')
-		return (ft_hex(va_arg((*arg), unsigned long int),
+		return (ft_hex_base(va_arg((*arg), unsigned long int),
 				"0123456789abcdef", -1));
 	else if (format == 'd' || format == 'i')
 		return (ft_putnbr(va_arg((*arg), int)));
 	else if (format == 'u')
 		return (ft_putnbr(va_arg((*arg), unsigned int)));
 	else if (format == 'X')
-		return (ft_hex(va_arg((*arg), unsigned int),
+		return (ft_hex_base(va_arg((*arg), unsigned int),
 				"0123456789ABCDEF", 0));
 	else if (format == 'x')
-		return (ft_hex(va_arg((*arg), unsigned int),
+		return (ft_hex_base(va_arg((*arg), unsigned int),
 				"0123456789abcdef", 0));
 	else if (format == '%')
 		return (ft_putchar('%'));
@@ -51,7 +51,7 @@ static	int	ft_print(va_list *arg, const char *str)
 	{
 		if (str[i] == '%')
 		{
-			de_len = ft_format(arg, str[++i]);
+			de_len = ft_format_check(arg, str[++i]);
 			if (de_len == -1)
 				return (-1);
 			len += de_len - 1;
@@ -72,12 +72,4 @@ int	ft_printf(const char *str, ...)
 	len = ft_print(&arg, str);
 	va_end(arg);
 	return (len);
-}
-
-#include <stdio.h>
-int main()
-{
-    char a[] = "0x1A3";
-    printf("%p\n",&a);
-    ft_printf("%p",&a);
 }
